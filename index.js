25a96db3aa0230fb28405d7bd513afee19ad3af0
abdumalik2014 <abdumalik2014@gmail.com>
2020-02-28 12:24:44 +0500

    feat: add  Factory Function and Constructor

--- index.js
+++ index.js
@@ -10,6 +10,36 @@ function createCircle(radius) {
 
 const circle = createCircle(1);
 
-//instead of writing 'radius: radius' we can simply write radius
-//draw is a property of which value is the function
-//draw is  method
+//Constructor function
+function Circle(radius) {
+  console.log("this", this);
+  this.radius = radius;
+  this.draw = function() {
+    console.log("draw");
+  };
+}
+
+//first letter should be uppercase
+//looks like class, but in JS, we dont have class
+//now, no need for object/proeprties
+//we use 'this' to set the properties of this object
+//reference to object that is executing this piece of code
+
+const another = new Circle(1);
+//new Circle - new operator to call Circle function
+//when we use new operator:
+//new operator creates new object - {}
+//it will set 'this' to point to that object
+//by default this, points to global object (window/global)
+
+//when we use new operator to call a function, 3 things happen
+//1. new operator creates new object - {}
+//2. it will set 'this' to point to that object
+//3.it will return that object from the function (Circle)
+
+//even though we are not writing explicitly 'return this'
+//it happens automatically, when we use new operator
+
+
+
+//2 ways of creating an object: Factory Function and Constructor
---
+++ test.js
@@ -0,0 +1,4 @@
+// for (const key in this) {
+//   console.log(key);
+// }
+console.log("this", this);
