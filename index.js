455ab7696092f35f7aa103e79fee644981525c35
abdumalik2014 <abdumalik2014@gmail.com>
2020-02-28 11:54:25 +0500

    feat: add factory funnction

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  draw: function() {
-    console.log("draw");
-  }
-};
+//Factory function
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function() {
+      console.log("draw");
+    }
+  };
+}
