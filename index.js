8baa93c0564593d93bbd3bddfd54c199f44ad04c
abdumalik2014 <abdumalik2014@gmail.com>
2020-02-28 11:58:55 +0500

    feat: add definition

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function createCircle(radius) {
     }
   };
 }
+
+const circle = createCircle(1);
+
+//instead of writing 'radius: radius' we can simply write radius
+//draw is a property of which value is the function
+//draw is  method
