a9c9d936b84fbb4afbc27a7afa5f72f6168643a7
abdumalik2014 <abdumalik2014@gmail.com>
2020-02-28 20:03:55 +0500

    feat: add part3 of Functions are objects: apply method of Function

--- index.js
+++ index.js
@@ -5,17 +5,9 @@ function Circle(radius) {
   };
 }
 
-// .call is used to call the function
-//1 arg: this argument. pass empty {}. 'this' will reference this empty object that we pass.
-//2 arg: radius
-
-// Circle.call({}, 1) IS SAME AS  const another = new Circle(1);
+// .apply is exactly like .call >> we can call the function
+//but instead of passing all the args explicitly (1, 2, 3), we pass them in an array
 Circle.call({}, 1);
-
-//when we use new operator, new operator internally creates empty object and pass that as the first arg to the call() method 
-//and this object (empty) will determine the context for 'this' (so this will reference {} )
-
-//if we dont use new, this by default will refer to global object >> window
-//// Circle.call(window, 1) IS SAME AS  const another = Circle(1);
+Circle.apply({}, [1,2,3]);
 
 const another = new Circle(1);
