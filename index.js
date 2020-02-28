184ccfca68b95ee8315db7295629b984d370a2f3
abdumalik2014 <abdumalik2014@gmail.com>
2020-02-28 18:42:24 +0500

    feat: add first part Constructor property: definition

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+//Constructor property
+
+//every object in JS has a property called constructor 
+//and that references the function that was used to construct (or to create) that object
+
+//try in console: another.constuctor >> returns Circle definition 
+//try in console: circle.constuctor >> returns Object (first letter uppercase >> function, constructor) 
+
 //Factory function
 function createCircle(radius) {
   return {
@@ -19,27 +27,4 @@ function Circle(radius) {
   };
 }
 
-//first letter should be uppercase
-//looks like class, but in JS, we dont have class
-//now, no need for object/proeprties
-//we use 'this' to set the properties of this object
-//reference to object that is executing this piece of code
-
 const another = new Circle(1);
-//new Circle - new operator to call Circle function
-//when we use new operator:
-//new operator creates new object - {}
-//it will set 'this' to point to that object
-//by default this, points to global object (window/global)
-
-//when we use new operator to call a function, 3 things happen
-//1. new operator creates new object - {}
-//2. it will set 'this' to point to that object
-//3.it will return that object from the function (Circle)
-
-//even though we are not writing explicitly 'return this'
-//it happens automatically, when we use new operator
-
-
-
-//2 ways of creating an object: Factory Function and Constructor
