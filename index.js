5786181aa47e0f3eb51ffe6dec2fd30c022833c4
abdumalik2014 <abdumalik2014@gmail.com>
2020-02-28 20:24:52 +0500

    feat: add part1 value types vs reference types

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-function Circle(radius) {
-  this.radius = radius;
-  this.draw = function() {
-    console.log("draw");
-  };
-}
+//value types: Number, String, Boolean, Symbol, undefined, null
+//reference types: Object, Function, Array
 
-// .apply is exactly like .call >> we can call the function
-//but instead of passing all the args explicitly (1, 2, 3), we pass them in an array
-Circle.call({}, 1);
-Circle.apply({}, [1,2,3]);
+let x = 10;
+let y = x;
 
-const another = new Circle(1);
+x = 20;
+
+//x & y are two independent variables
+// x = 20 && y = 10
+
+//when we work with primitives, the value is saved inside of the variable
+//when we copy that variable (let y = x), the value stored in the variable is copied into the new variable (y)
+//they are completely independent of each other
+
+let a = { value: 10 };
+let b = a;
+
+a.value = 20;
+
+//a.value = 20 && b.value =20
+
+//when we use an object, that object is not stored in the variable (a)
+//that object is stored in the memory, and the address of that memory location is stored inside that variable
+
+//when we copy a to b, its the address(reference) that is copied
+//both a & b pointing to the same object in the memory 
+//when we modify that object using a OR b, the changes is immediately visible to other variable
+
+//Primitives are copied by their value
+//Objects are copied by their reference
