ec8883d7338782714d663ddf2b74719ff2435d19
abdumalik2014 <abdumalik2014@gmail.com>
2020-03-01 22:41:45 +0500

    fix: add Stopwatch author's solution

--- exercise.js
+++ exercise.js
@@ -1,35 +1,39 @@
 function Stopwatch() {
   let startTime,
     endTime,
-    started = false,
-    stopped = false,
+    running,
     duration = 0;
+
+  this.start = function() {
+    if (running) {
+      throw new Error("Stopwatch has already started");
+    }
+    running = true;
+
+    startTime = new Date();
+  };
+
+  this.stop = function() {
+    if (!running) {
+      throw new Error("Stopwatch is not started");
+    }
+    running = false;
+
+    endTime = new Date();
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    duration += seconds;
+  };
+
+  this.reset = function() {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
 
   Object.defineProperty(this, "duration", {
     get: function() {
       return duration;
-    },
-    set: function(value) {
-      duration = value;
     }
   });
-
-  this.start = function() {
-    if(started){
-        throw new Error('Stopwatch has already started')
-    }
-    startTime = Date.now();
-    started = true;
-  };
-  this.stop = function() {
-    if(stopped){
-        throw new Error('Stopwatch is not started')
-    }
-    endTime = Date.now();
-    duration = endTime - startTime;
-    stopped = true;
-  };
-  this.reset = function() {
-    duration = 0;
-  };
 }
