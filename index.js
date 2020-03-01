07ef2a527dd56bf50e31d65c2f27c5270c7f747d
abdumalik2014 <abdumalik2014@gmail.com>
2020-03-01 17:03:45 +0500

    feat: add Private Properties and Methods

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
+//this references that new object
 function Circle(radius) {
+  let color = "red";
+  //is this a part of Circle object?
+  //No. we haven't set it as a property of that object: this.color = color;
+  //when we get out of this function, this variable gets out of scope and dies
+  //with this technique, we can hide certain members from outside
+
   this.radius = radius;
-  this.defaultLocation = { x: 0, y: 0 };
-  this.computerOptimumLocation = function(factor) {
+
+  //defaultLocation is implementation detail, we dont want it to be accessible from outside
+  //this.default >> let default
+  let defaultLocation = { x: 0, y: 0 };
+
+  //turn into private method: this.compute >> let compute
+  let computeOptimumLocation = function(factor) {
     //...
   };
 
   this.draw = function() {
-    this.computerOptimumLocation(0.1);
+
+    //this function will no longer be the method of 'this' new object
+    //this.compute >> compute
+    //it works because in JS there is 'closure'
+    //we have function (Circle) and another function (draw) which is inside of that function
+
+    computeOptimumLocation(0.1);
+
+    let x, y;
+    //x, y available only inside draw function >> their scope is limited to this function
+    //contrast of scope >> closure
+    //closure determines what variables are accessible to an inner function 
+    //draw can access all the local variables (x, y) as well as the variables defined in its parent function (compute, default)   
+
+
+    //scope is temporary >> var x, y dies and recreated in each call
+    //closure stays there
+
+    //to access private members of Circle object
+    defaultLocation.x = 5;
+
+    //to access members of the new Circle object
+    this.radius = 20; 
+
     console.log("draw");
   };
 }
 
 const circle = new Circle(10);
-//problem with 'this' inside 'this'
-//circle. >> shows all: radius, draw, defaultLocation, computerOptimumLocation
-//not all members should be accessible to me, as the consumer (client) of the object
-//e.g. what happens if I set default location to false: it messes up everything
-circle.defaultLocation = false;
-
-//OR maybe compute() method should only be called inside draw() method
-
-circle.computerOptimumLocation();
-//maybe this will put object in a bad state and ...
-
+//if we access members of circle object >> circle. >> only draw and radius
+circle.
 circle.draw();
-//can cause to weird error at runtime
-
-// >> Abstraction
-// Hide the details (complexity) and Show the essentials
-
-// Hide defaultLocation and compute() method
-// these are implementation details, these are complexity of object
-// Hide them from the consumers of this object
-// Show only essentials >> radius and draw()
-
-//DVD metaphor: inside & outside
-
-//because computerOptimumLocation() is accessible outside, if later we add parameter to this function (factor), we should pass parameter to its call also
-//it results to number of changes in the code
-//imagine this method was not accessible from the outside.
-//then we would change only one line, when its called (line 9)
